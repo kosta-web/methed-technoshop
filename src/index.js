85154dea7f935b3ca65e97f89453f2723fa5131e
podyankee <podyankee@gmail.com>
2022-07-03 06:27:14 +0300

    day-3

--- src/index.js
+++ src/index.js
@@ -2,3 +2,37 @@ import './index.html';
 import './cart.html';
 import './card.html';
 import './index.scss';
+
+import Swiper, { Thumbs, Scrollbar, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/pagination';
+const thumbsSwiper = new Swiper('.card__slider-thumb', {
+	spaceBetween: 44,
+	slidesPerView: 3,
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		draggable: true,
+	},
+	modules: [Scrollbar],
+});
+
+new Swiper('.card__image', {
+	spaceBetween: 10,
+	slidesPerView: 1,
+	thumbs: {
+		swiper: thumbsSwiper,
+		slideThumbActiveClass: 'card__thumb-btn_active',
+	},
+	modules: [Thumbs],
+});
+
+new Swiper('.recommended__carousel', {
+	spaceBetween: 30,
+	slidesPerView: 5,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	modules: [Pagination],
+});
