d742f7693b1493a50bae35055eb1b1985fd1e311
podyankee <podyankee@gmail.com>
2022-07-05 05:28:19 +0300

    day-4

--- src/index.js
+++ src/index.js
@@ -7,6 +7,23 @@ import Swiper, { Thumbs, Scrollbar, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 import 'swiper/css/pagination';
+
+import { pagination } from './modules/pagination';
+
+const paginationWrapper = document.querySelector('.pagination');
+
+const pageUrl = new URL(location);
+pageUrl.searchParams.get('page');
+
+const page = +pageUrl.searchParams.get('page') || 1;
+
+try {
+	pagination(paginationWrapper, 20, page, 6);
+} catch (e) {
+	console.warn(e);
+	console.warn('Это не главная страница');
+}
+
 const thumbsSwiper = new Swiper('.card__slider-thumb', {
 	spaceBetween: 44,
 	slidesPerView: 3,
